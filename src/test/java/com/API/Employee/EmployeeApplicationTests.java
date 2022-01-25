365bd9407b506e7c7e87344540a2f43b024e2f4e
Shorlak <shorlakdev@gmail.com>
2022-01-25 17:51:09 -0500

    first commit

---
+++ src/test/java/com/API/Employee/EmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.API.Employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
